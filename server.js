a8587672f00ca52e7ba7694a30d2b10cec2bb216
Michal Plewa <themipsial@gmail.com>
2022-08-24 03:48:17 +0200

    add router to server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
+const adRouter = require('./routes/ad.route');
+const userRouter = require('./routes/user.route');
+
 const app = express();
-app.listen(process.env.PORT || 8000, () => {
-  console.log('server is running on port 8000');
-});
 
 // conection to db
 
@@ -16,3 +16,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.use('/api', adRouter);
+app.use('/api', userRouter);
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Not found...' });
+});
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log('server is running on port 8000');
+});
