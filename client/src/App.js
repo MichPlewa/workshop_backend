6e77ce0d0fa08f99d2567cb5dc7ad15300d6dc86
Michal Plewa <themipsial@gmail.com>
2022-08-30 14:51:26 +0200

    header in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
+import { Container } from 'react-bootstrap';
+import Header from './colection/view/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './colection/Pages/Home/Home';
+
 const App = () => {
-  return <div>Hej</div>;
+  return (
+    <main>
+      <Container>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Container>
+    </main>
+  );
 };
 
 export default App;
---
+++ client/src/colection/view/NavBar/NavBar.js
@@ -0,0 +1,26 @@
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <Navbar
+      bg="primary"
+      variant="dark"
+      expand="lg"
+      className="mt-4 mb-4 rounded"
+    >
+      <Container>
+        <Navbar.Brand>Ad</Navbar.Brand>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="justify-content-end flex-grow-1 pe-3">
+            <Nav.Link to="/" as={NavLink}>
+              Home
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
